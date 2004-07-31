f95926ebcbd9ba42b59b939662aea5b813477de9
uls <uls>
2004-07-31 08:58:49 +0000

    website cleanup

--- CCAPI/javadoc.sh
+++ CCAPI/javadoc.sh
@@ -1,4 +1,3 @@
-javadoc -windowtitle 'CCAPI JavaDocs' -splitindex -header '<b>CCAPI</b>' -bottom '(c) 2002-2004 Ulrich Staudinger' -sourcepath ./src -d ./web/javadoc CCAPI 
+javadoc -windowtitle 'CCAPI JavaDocs' -splitindex -header '<b>CCAPI</b>' -bottom '(c) 2002-2004 Ulrich Staudinger' -sourcepath ./src -d ./javadoc CCAPI 
 
-javadoc -windowtitle 'CCAPI JavaDocs' -splitindex -header '<b>CCAPI</b>' -bottom '(c) 2002-2004 Ulrich Staudinger' -sourcepath ./src -d /opt/apache/htdocs/CCAPI CCAPI 
 
