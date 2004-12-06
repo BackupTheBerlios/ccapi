a826071e46d1598687f366886809c480ae575d1d
uls <uls>
2004-12-06 17:03:53 +0000

    public added. typo.

--- CCAPI/src/CCAPI/EuwaxQuoteRetriever.java
+++ CCAPI/src/CCAPI/EuwaxQuoteRetriever.java
@@ -10,7 +10,7 @@ import java.net.URL;
  *
  */
 public class EuwaxQuoteRetriever {
-    EuwaxQuoteRetriever() {
+    public EuwaxQuoteRetriever() {
     }
 
     public double[] retreiveQuote(String wkn) {
