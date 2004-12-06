343e4f2fbdf1d82a5903c940c7bbe5dfc88a65e4
uls <uls>
2004-12-06 17:27:56 +0000

    fixed EUWAXQuoteRetriever

--- CCAPI/src/CCAPI/EuwaxQuoteRetriever.java
+++ CCAPI/src/CCAPI/EuwaxQuoteRetriever.java
@@ -13,6 +13,11 @@ public class EuwaxQuoteRetriever {
     public EuwaxQuoteRetriever() {
     }
 
+    public static void main(String[] args){
+		EuwaxQuoteRetriever e=new EuwaxQuoteRetriever();
+		e.retreiveQuote("BNP17D");
+    }
+    
     public double[] retreiveQuote(String wkn) {
         System.gc();
 
@@ -20,7 +25,7 @@ public class EuwaxQuoteRetriever {
 
         try {
             URL u = new URL(
-                    "http://www.rt.boerse-stuttgart.de/pages/details/main.html?action=overview&sSymbol=" +
+			"http://www.rt.boerse-stuttgart.de/pages/search/main.html?searchterm="+
                     wkn);
 
             // http://isht.comdirect.de/html/detail/ticklist/main.html?sSym=DE0009535245.C41
@@ -29,28 +34,29 @@ public class EuwaxQuoteRetriever {
             int line = 1;
 
             while (l != null) {
-                l = l.trim();
+		//System.out.println("line: "+line+" : "+ l);
+		l = l.trim();
 
-                if (line == 84) {
+                if (line == 112) {
                     // the line containing the everyfuck
-                    int i1 = l.indexOf("Bid : ");
+                    int i1 = l.indexOf("Geld:");
                     int i2 = l.indexOf("Quotierungszeitpunkt");
 
-                    l = l.substring(i1, l.length() - i2);
+                    l = l.substring(i1+5, l.length() - i2);
 
                     // crop the Bid : 
                     l = l.substring(6);
-
+			
                     int i3 = l.indexOf("</td>");
                     String bid = l.substring(0, i3);
 
-                    System.out.println("Extracted <" + bid + ">");
+                    //System.out.println("Extracted <" + bid + ">");
 
-                    int i4 = l.indexOf("Ask");
+                    int i4 = l.indexOf("Brief:");
 
-                    System.out.println("i4: " + i4);
-                    l = l.substring(i4 + 6, l.length() - i4 - 6);
-                    System.out.println("l <" + l + ">");
+                    //System.out.println("i4: " + i4);
+                    l = l.substring(i4 + 6 + 6, l.length() - i4 - 6);
+                    //System.out.println("l <" + l + ">");
 
                     int i5 = l.indexOf("</td>");
                     String ask = l.substring(0, i5);
