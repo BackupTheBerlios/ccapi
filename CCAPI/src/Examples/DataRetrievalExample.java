65894c2333cd8738154fe64a2005a99491eb306e
uls <uls>
2005-02-24 14:13:32 +0000

    initial example

---
+++ CCAPI/src/Examples/DataRetrievalExample.java
@@ -0,0 +1,16 @@
+/*
+ * Created on 24.02.2005
+ * 
+ * GPL protected.
+ * Author: Ulrich Staudinger
+ * 
+ */
+package Examples;
+
+
+public class DataRetrievalExample {
+
+	public static void main(String[] args) {
+		
+	}
+}
